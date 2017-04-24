98dbbc4d5ca22612f85f1485f7d72bd0656ed541
Tropsis <Matias.saila@gmail.com>
2017-04-24 13:40:02 +0300

    Muutoksia
    Linkkauksia kuntoon. Href

--- contact.php
+++ contact.php
@@ -24,8 +24,8 @@
 			<ul class="topnav">
 
             <li><a href="tokasivu2.php">Patient list</a></li>
-            <li><a class="add_patient" href="add_patient.php">Add patient</a></li>
-            <li><a class="delete_patient" href="delete_patient.php">Delete patient</a></li>
+            <li><a class="add_patient" href="add.php">Add patient</a></li>
+            <li><a class="delete_patient" href="deletepatient.php">Delete patient</a></li>
             <li><a href="about.php">About</a></li>
             <li><a href="contact.php">Contact</a></li>
             <li class="right"><a href="ulos.php">Log out</a></li>
