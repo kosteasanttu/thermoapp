4f9abed1f89d8608ad3b096bad264988b0560d14
kosteaviljami <koho.viljami@hotmail.com>
2017-04-26 12:27:27 +0300

    selitykset lämpötilalle

--- tokasivu2.php
+++ tokasivu2.php
@@ -30,6 +30,13 @@
             <li class="right"><a href="ulos.php">Log out</a></li>
             </ul>
 		</div><!-- #header -->
+
+        <div class="lomake_selitys">
+            <h4><span style="color:red">Punainen tila</span> tarkoittaa, että potilaan lämpötila on ylittänyt kriittisen lämpötila-arvon 39°C</h4>
+            <h4><span style="color:yellow">Keltainen tila</span> tarkoittaa, että potilaan lämpötila on ylittänyt normaalin kuumerajan +37°C</h4>   
+            <h4><span style="color:green"> Vihreä tila</span> tarkoittaa, että potilaan lämpötila on normaalien lämpötila-arvojen sisällä</h4>
+        </div>
+        
         <div id="refresh">
         <button class="submit" onclick="window.location.href='http://users.metropolia.fi/~santtumk/thermoapp/mySignalsexample.php'">Refresh</button>
         </div>
