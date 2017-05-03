9ada11e671a53bd67296a70718e74a9f8239d213
kosteaviljami <koho.viljami@hotmail.com>
2017-05-03 13:16:31 +0300

    about sivun kuvan leveys muutettu

--- about.php
+++ about.php
@@ -34,7 +34,7 @@
 		</div>
 
 		<div class="nurse_vasen">
-			<img src="img/nurse.jpg" width="600" height="400">
+			<img src="img/nurse.jpg" width="550" height="400">
 		</div>
 
 		<div class="about">
