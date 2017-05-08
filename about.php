55fa60eb11b353e9c5652ab352a38440a050e57e
kosteaviljami <koho.viljami@hotmail.com>
2017-05-08 11:55:06 +0300

    div id banner pois ja altti kuvalle

--- about.php
+++ about.php
@@ -21,7 +21,6 @@
 	</a>
 	
 	<div id="wrapper">
-		<div id="banner"></div>
 			
 		<div id="header">
 			<ul class="topnav">
@@ -35,7 +34,7 @@
 		</div>
 
 		<div class="nurse_vasen">
-			<img src="img/nurse.jpg" width="550" height="400">
+			<img alt="hoitaja" src="img/nurse.jpg" width="550" height="400">
 		</div>
 
 		<div class="about">
